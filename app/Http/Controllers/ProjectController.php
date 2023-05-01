4ac3125e673b3e69a07f05cd2bd268d47795071c
MaionePietro <pietromaionespam@gmail.com>
2023-05-02 00:06:24 +0200

    first commit

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Project;
+use App\Models\Type;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::withTrashed()->get()   ;
+        return view('project.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $types = Type::orderBy('name', 'asc')->get();
+
+        return view('project.create', compact('types'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreProjectRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        dd($request);
+        $data = $request->validated();
+        $data['slug'] = Str::slug($data['title']);
+        $project = Project::create($data); //premdo dal model
+        return to_route('projects.show',$project);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        return view('project.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Project $project)
+    {
+        $types = Type::orderBy('name', 'asc')->get();
+        return view('project.edit', compact('project','types'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateProjectRequest  $request
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        $data = $request->validated();
+        if($data['title'] !== $project->title){
+
+            $data['slug'] = Str::slug($data['title']);
+        }
+        $project->update($data);
+        return to_route('projects.show', $project);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+        if ($project->trashed()) {
+            $project->forceDelete(); // eliminazione def
+        } else {
+            $project->delete(); //eliminazione soft
+        }
+        return back();
+    }
+
+    public function restore(Project $project)
+    {
+        if ($project->trashed()) {
+            $project->restore(); // eliminazione def
+        }
+        return back();
+    }
+}
---
+++ resources/views/project/edit.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <form action="{{ route('projects.update', $project) }}" method="POST">
+
+        @csrf
+        @method('put')
+        <div class="mb-3">
+            <label for="title" class="form-label">title</label>
+            <input type="text" class="form-control" @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title',$project->title) }}">
+            @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+              @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="type-id" class="form-label">Categoria</label>
+            <select class="form-select @error('type_id') is-invalid @enderror" id="type-id" name="type_id" aria-label="Default select example">
+              <option value="" selected>Seleziona categoria</option>
+              @foreach ($types as $type)
+                <option @selected( old('type_id', $project->type_id) == $type->id ) value="{{ $type->id }}">{{ $type->name }}</option>
+              @endforeach
+            </select>
+            @error('project_id')
+              <div class="invalid-feedback">
+                  {{ $message }}
+              </div>
+            @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="customer" class="form-label">customer</label>
+            <input type="text" class="form-control" @error('customer') is-invalid @enderror" id="customer" name="customer" value="{{ old('customer',$project->customer)}}">
+            @error('customer')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+              @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="url" class="form-label">url</label>
+            <input type="text" class="form-control" @error('url') is-invalid @enderror" id="url" name="url" value="{{ old('url',$project->url) }}">
+            @error('url')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+              @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="description" class="form-label">description</label>
+            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">
+                {{ old('description',$project->description) }}
+            </textarea>
+            @error('description')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+              @enderror
+        </div>
+
+        <button type="submit" class="btn btn-primary">Salva</button>
+
+    </form>
+</div>
+
+@endsection
